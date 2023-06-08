918fe35e57a920df26c9dbe92d8ccdfe3f2c5f02
Erik Sandoval <eriksandoval.py@gmail.com>
2023-06-08 00:06:04 -0500

    Began adding private fields to Item.java.

--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,15 @@
 public class Item {
+    private String name;
+    private String description;
+    private int price;
+    private int quantity;
+    private int cost;
+    private int sku;
+    private int weight;
+    private int length;
+    private int width;
+    private int height;
+    private int category;
+    private int subcategory;
     
 }
