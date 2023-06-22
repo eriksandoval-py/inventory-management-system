a684fbe60dc6b95555ae5b9aa4e28f2f842ccd3b
Erik Sandoval <eriksandoval.py@gmail.com>
2023-06-22 16:44:21 -0500

    Scoped as prototype pattern.

--- src/main/java/com/eriksandoval/inventorymanagementsystem/collection/LeadTechnician.java
+++ src/main/java/com/eriksandoval/inventorymanagementsystem/collection/LeadTechnician.java
@@ -1,8 +1,10 @@
 package com.eriksandoval.inventorymanagementsystem.collection;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class LeadTechnician implements Employee, Observer {
     
         @Override
