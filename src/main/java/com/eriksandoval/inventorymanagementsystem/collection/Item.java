9076c14ee532f02bac33a1a105bb997de33d1a4d
Erik Sandoval <eriksandoval.py@gmail.com>
2023-06-22 16:01:15 -0500

    Added getters and setters for Item.

--- src/main/java/com/eriksandoval/inventorymanagementsystem/collection/Item.java
+++ src/main/java/com/eriksandoval/inventorymanagementsystem/collection/Item.java
@@ -13,5 +13,71 @@ public class Item {
     private int height;
     private int category;
     private int subcategory;
-    
+
+    public String getName() {
+        return name;
+    }
+    public void setName() {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription() {
+        this.description = description;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice() {
+        this.price = price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity() {
+        this.quantity = quantity;
+    }
+    public int getCost() {
+        return cost;
+    }
+    public void setCost() {
+        this.cost = cost;
+    }
+    public int getSku() {
+        return sku;
+    }
+    public void setSku() {
+        this.sku = sku;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight() {
+        this.weight = weight;
+    }
+    public int getLength() {
+        return length;
+    }
+    public void setLength() {
+        this.length = length;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth() {
+        this.width = width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight() {
+        this.height = height;
+    }
+    public int getCategory() {
+        return category;
+    }
+    public void setCategory() {
+        this.category = category;
+    }
 }
