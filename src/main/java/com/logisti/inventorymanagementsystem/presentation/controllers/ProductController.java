12be223dfa4b1eef6b7ad4c3a6a43006dd2b8bfa
Erik Sandoval <eriksandoval.py@gmail.com>
2023-07-03 10:58:06 -0500

    Controller imports.

--- src/main/java/com/logisti/inventorymanagementsystem/presentation/controllers/ProductController.java
+++ src/main/java/com/logisti/inventorymanagementsystem/presentation/controllers/ProductController.java
@@ -1,6 +1,10 @@
 package com.logisti.inventorymanagementsystem.presentation.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
